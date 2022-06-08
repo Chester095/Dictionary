f4b187c8d8a440778f1cf92006a990cce855c275
geekbrains <chester_095@mail.ru>
2022-06-08 17:44:48 +0300

    HW2 - начал переделывать на MVVM

--- app/src/main/java/com/geekbrains/dictionary/ui/MainActivity.kt
+++ app/src/main/java/com/geekbrains/dictionary/ui/MainActivity.kt
@@ -5,28 +5,32 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.utils.ViewState
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.geekbrains.dictionary.R
 import com.geekbrains.dictionary.databinding.ActivityMainBinding
 import com.geekbrains.dictionary.domain.words.WordsEntity
 
 
-class MainActivity : AppCompatActivity(), MainActivityContract.MainActivityView{
+class MainActivity : AppCompatActivity(), MainActivityContract.ViewModel{
 
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
     private val myAdapter by lazy { MainActivityAdapter() }
     private val progressDialog by lazy { ProgressDialog(this) }
-    private var presenter: MainActivityContract.MainActivityPresenter = MainActivityPresenter(this)
+//    private var presenter: MainActivityContract.MainActivityPresenter = MainActivityPresenter(this)
+    private val VIEW_STATE_KEY = "VIEW_STATE_KEY"
+    private var viewState: ViewState = ViewState.INIT
+    private var viewModel: MainActivityContract.ViewModel?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setTheme(R.style.MyThemeOrange)
         setContentView(binding.root)
-        presenter.attachView(this)
-
+//        presenter.attachView(this)
+viewModel?.loadDataFromRepo().sub
         initRecyclerView()
 
         binding.inputLayout.setEndIconOnClickListener {
@@ -39,7 +43,7 @@ class MainActivity : AppCompatActivity(), MainActivityContract.MainActivityView{
         binding.recyclerView.adapter = myAdapter
     }
 
-    override fun showListWordsTranslated(list: List<WordsEntity>) {
+    fun showListWordsTranslated(list: List<WordsEntity>) {
         myAdapter.refreshList(list)
         Log.d("!!!", "showListTranslated: ")
     }
--- app/src/main/java/com/geekbrains/dictionary/ui/MainActivityContract.kt
+++ app/src/main/java/com/geekbrains/dictionary/ui/MainActivityContract.kt
@@ -4,17 +4,7 @@ import com.geekbrains.dictionary.domain.words.WordsEntity
 
 class MainActivityContract {
 
-    interface MainActivityView{
-        fun showListWordsTranslated(list:List<WordsEntity>)
-        fun showError()
-        fun startShowProgressLoading()
-        fun stopShowProgressLoading()
-
-    }
-
-    interface MainActivityPresenter{
-        fun attachView(view: MainActivityView)
-        fun detachView()
+    interface ViewModel{
         fun requestTranslated(searchWord: String)
         fun loadDataFromRepo()
     }
