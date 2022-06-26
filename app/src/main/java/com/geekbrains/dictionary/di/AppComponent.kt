eb4d91d97a94c853349e902367c992bffd720981
Chester095 <Chester095@users.noreply.github.com>
2022-06-26 21:54:46 +0300

    HW2 - gthtltksdf. d MVVM

--- app/src/main/java/com/geekbrains/dictionary/di/AppComponent.kt
+++ app/src/main/java/com/geekbrains/dictionary/di/AppComponent.kt
@@ -5,8 +5,14 @@ import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
+// Singleton — это базовая аннотация, предоставляемая Dagger. Она предоставляется, чтобы было от
+//чего отталкиваться. Появление Singletone не произойдёт магическим образом, если не сохранить свой
+//AppComponent в классе App — классе, расширяющем Application.
 @Component(modules = [NetworkModule::class, StorageModule::class])
+// @Component отмечает интерфейс, который позже используется для генерации кода. В нём
+// определяется, куда что-либо внедрять, а также методы для прямого доступа к зависимостям
 interface AppComponent {
+    // injest - внедрить
     fun injectMainActivityPresenter(mainActivityPresenter: MainActivityPresenter)
 
 
